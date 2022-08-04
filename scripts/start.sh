9291c97e9956be77a6086cccf1643d934bd77875
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-08-04 18:11:25 +0900

    feat:log 권한 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "[App Start]======================"
-cd /home/ec2-user/lgcns_data_manager
+cd /home/ec2-user/lgcns_api_server
+sudo touch .log
+sudo chmod 777 .log
 nohup java -jar build/libs/lgcns_api_server*.jar > .log &
