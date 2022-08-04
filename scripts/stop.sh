45b818174f5be9412ed557ca1797cbf981da0fb2
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-08-04 14:38:37 +0900

    feat:stop script 추가

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 echo "[App Stop]======================"
+PROJECT_NAME=lgcns_api_server
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep jar)
+
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+if [ -z "$CURRENT_PID" ]; then
+        echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+        echo "> kill -15 $CURRENT_PID"
+        kill -15 $CURRENT_PID
+        sleep 5
+fi
