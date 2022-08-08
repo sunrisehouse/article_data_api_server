784d460fce910d51dad105d72d5a524ff2e9698c
HanJungwoo1102 <ajtwlswjddnv1102@gmail.com>
2022-08-08 15:22:29 +0900

    feat:start script 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-echo "[App Start]======================"
-cd /home/ec2-user/lgcns_api_server
+echo "[Start Script]===================================="
+APP_HOME=/home/ec2-user/lgcns_api_server
+
+echo "App Home: $APP_HOME"
+echo "Who Am I:"
+whoami
+
+echo "[Edit App Folder Permission]"
+sudo chown -R ec2-user:ec2-user $APP_HOME
+cd $APP_HOME
+
+echo "[Start Application]"
 nohup java -jar build/libs/lgcns_api_server*.jar &
+
